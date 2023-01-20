e86128070bdb73761add97e84a03d85830d59ec4
Kerem VAHAPOĞLU <21060294@stu.omu.edu.tr>
2023-01-20 23:16:14 +0300

    kar zarar raporu oluşturuldu ve tamamlandı

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,19 @@ using System.Runtime.InteropServices;
 // Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
 // denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
 // bu öznitelik değerlerini değiştirin.
+<<<<<<< HEAD
 [assembly: AssemblyTitle("Satış")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Satış")]
+=======
+[assembly: AssemblyTitle("kar_zarar_rapor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kar_zarar_rapor")]
+>>>>>>> e9a4d3f (kar zarar raporu oluşturuldu ve tamamlandı)
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +28,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+<<<<<<< HEAD
 [assembly: Guid("2cc4fa5d-0bc7-490a-9226-b937b68e71a6")]
+=======
+[assembly: Guid("f9a614cf-519f-42b6-95a1-451ffc7d6ebd")]
+>>>>>>> e9a4d3f (kar zarar raporu oluşturuldu ve tamamlandı)
 
 // Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
 //
